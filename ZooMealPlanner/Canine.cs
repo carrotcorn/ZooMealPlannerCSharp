faff26dfe613bc0a0c6aa0826c8ec204f044699c
carrotcorn <47840684+carrotcorn@users.noreply.github.com>
2019-11-24 19:00:39 -0800

    added write lines to the canine class for selection

--- ZooMealPlanner/Canine.cs
+++ ZooMealPlanner/Canine.cs
@@ -6,5 +6,18 @@ namespace ZooMealPlanner
 {
    internal sealed class Canine : Mammal
     {
+        public void GetCanine()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*******************************");
+            Console.WriteLine("Breed");
+            Console.WriteLine("*******************************");
+            Console.WriteLine("1. Timber Wolf");
+            Console.WriteLine("2. Grey Wolf");
+            Console.Write("What is your selection     ");
+
+            
+        }
+
     }
 }
