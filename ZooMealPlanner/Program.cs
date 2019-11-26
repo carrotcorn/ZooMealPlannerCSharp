f22250f18caffc25e05670e32dd95f12b6899463
carrotcorn <47840684+carrotcorn@users.noreply.github.com>
2019-11-25 16:42:05 -0800

    restarted from scratch and am rebuilding my program class

--- ZooMealPlanner/Program.cs
+++ ZooMealPlanner/Program.cs
@@ -2,12 +2,30 @@
 
 namespace ZooMealPlanner
 {
-    internal class Program
+    class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Menu.Display();
+            // Declare variables and then initialize to zero.
+            bool close = false;
 
+            while (!close)
+            {
+                // Display title as the C# Zoo Meal Planner 
+                Console.WriteLine("Zoo Meal Planner");
+                Console.WriteLine("************************");
+
+                // Ask the user to choose an option.
+                Console.WriteLine("Choose an animal : ");
+                Console.WriteLine(" 1 - Monkey");
+                Console.WriteLine(" 2 - Bear");
+                Console.WriteLine(" 3 - Quit");
+                Console.Write("What is your selection? ");
+
+                string selection = Console.ReadLine();
+
+                
+            }
         }
     }
 }
